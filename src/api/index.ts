7152ad2a587a03f9fedc3205a5ff75c934b2b5fa
Do Minh <dominh@Dos-MacBook-Air.local>
2025-03-02 05:07:34 +0700

    ghép extract mặt trước sau

--- src/api/index.ts
+++ src/api/index.ts
@@ -38,13 +38,8 @@ axiosPrivate.interceptors.response.use(
   }
 )
 
-// export const apiPostCall = async (url: string, data?: unknown, isPrivate?: boolean) => {
-//   const response = isPrivate ? await axiosPrivate.post(url, data) : await axios.post(url, data)
-//   return response
-// }
-
-export const apiPostCall = async (url: string, data?: unknown, config?: any) => {
-  const response = await axiosPrivate.post(url, data, config)
+export const apiPostCall = async (url: string, data?: unknown, isPrivate?: boolean) => {
+  const response = isPrivate ? await axiosPrivate.post(url, data) : await axios.post(url, data)
   return response
 }
 
@@ -58,12 +53,7 @@ export const apiDeleteCall = async (url: string, isPrivate?: boolean) => {
   return response
 }
 
-// export const apiGetCall = async (url: string, isPrivate?: boolean) => {
-//   const response = isPrivate ? await axiosPrivate.get(url) : await axios.get(url)
-//   return response
-// }
-
-export const apiGetCall = async (url: string, config?: any) => {
-  const response = await axiosPrivate.get(url, config)
+export const apiGetCall = async (url: string, isPrivate?: boolean) => {
+  const response = isPrivate ? await axiosPrivate.get(url) : await axios.get(url)
   return response
 }
