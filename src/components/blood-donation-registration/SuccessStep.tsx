f7ef0e38f210edaa69714068e1f0d0cf3e7272b7
leeanhde <114909280+leeanhde@users.noreply.github.com>
2025-03-19 22:51:56 +0700

    update link

--- src/components/blood-donation-registration/SuccessStep.tsx
+++ src/components/blood-donation-registration/SuccessStep.tsx
@@ -64,7 +64,7 @@ const SuccessStep: FC<SuccessStepProps> = ({ navigate }) => {
             >
               <Button
                 className='bg-red-600 hover:bg-red-700 text-white shadow transition-all duration-200 hover:shadow-lg flex items-center justify-center'
-                onClick={() => navigate('/lich-su-hien-mau')}
+                onClick={() => navigate('/trang-ca-nhan#lich-hen')}
               >
                 <Calendar className='mr-2 h-4 w-4' />
                 Xem lịch hẹn
