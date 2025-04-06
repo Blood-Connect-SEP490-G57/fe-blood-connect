8ee904bfdea6d2773ae1fc5cadaa5e0f398cb42d
leeanhde <114909280+leeanhde@users.noreply.github.com>
2025-04-07 04:51:25 +0700

    update dang ky

--- src/api/appointment/index.ts
+++ src/api/appointment/index.ts
@@ -19,3 +19,26 @@ export const getHistory = async (): Promise<any> => {
   })
   return response.data
 }
+
+export const updateinfor = async (payload: any): Promise<any> => {
+  const response = await axios.put(
+    '/api/appointments/user-info',
+    {
+      userId: payload.userId,
+      email: payload.email,
+      jobName: payload.jobName,
+      studentId: payload.studentId,
+      militaryId: payload.militaryId,
+      addressContact: payload.addressContact,
+      bloodGroup: payload.bloodGroup,
+      organizationId: payload.organizationId
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+      }
+    }
+  )
+  return response.data
+}
