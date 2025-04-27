9209f0f2ee0bdb09dcb372cde9993833c6553cb8
leeanhde <114909280+leeanhde@users.noreply.github.com>
2025-04-27 09:57:27 +0700

    Update Step4Complete.tsx

--- src/components/user-profile/Verification/Step4Complete.tsx
+++ src/components/user-profile/Verification/Step4Complete.tsx
@@ -28,8 +28,8 @@ const Step4Complete: React.FC<Step3CompleteProps> = ({ onRegisterClick }) => {
     <div className='space-y-6 mx-auto sm:p-4 p-2'>
       <ScrollToTop />
       <div className='flex flex-col items-center justify-center text-center mt-2 space-y-4'>
-        <h2 className='text-xl font-semibold text-gray-900'>Xác thực thành công</h2>
-        <p className='text-gray-600'>
+        <h2 className='text-xl font-semibold'>Xác thực thành công</h2>
+        <p>
           Chúc mừng! Bạn đã hoàn tất hồ sơ hiến máu. Bây giờ bạn có thể đăng ký các chiến dịch hiến máu.
         </p>
       </div>
@@ -37,25 +37,25 @@ const Step4Complete: React.FC<Step3CompleteProps> = ({ onRegisterClick }) => {
       <Card className='overflow-hidden rounded-xl shadow-sm border-none'>
         <CardContent className='p-5'>
           <div className='space-y-4'>
-            <h3 className='font-medium text-gray-900'>Thông tin tài khoản</h3>
+            <h3 className='font-medium'>Thông tin tài khoản</h3>
             <div className='flex items-center justify-between p-3 bg-green-50 rounded-lg'>
               <span className='text-sm text-green-700'>Trạng thái xác thực</span>
               <span className={`px-2 py-1 text-xs rounded-full font-medium `}>
                 {verificationStatus === 'EXTRACTED' ? 'Đã xác thực' : 'Chờ xác thực'}
               </span>
             </div>
-            <p className='text-sm text-gray-600'>
+            <p>
               Thông tin của bạn đã được xác thực và lưu trữ an toàn trong hệ thống. Bạn có thể cập nhật thông tin của
-              mình bất kỳ lúc nào trong mục Hồ sơ.
+              mình bất kỳ lúc nào trong mục thông tin cá nhân.
             </p>
           </div>
         </CardContent>
       </Card>
 
-      <div className='flex flex-col sm:flex-row sm:items-center sm:justify-between'>
+      <div className='flex flex-col sm:flex-row sm:items-center justify-end'>
         <button
           onClick={onRegisterClick}
-          className='bg-red-600 hover:bg-red-700 text-white p-2 rounded-xl transition-colors flex items-center justify-center gap-2'
+          className='bg-red-600 hover:bg-red-700 text-white p-4 rounded-xl transition-colors flex items-center justify-center gap-2'
         >
           <div>
             <span>Đăng ký hiến máu</span>
